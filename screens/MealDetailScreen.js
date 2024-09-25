47d160e75bd10bdfcf60772f7bc5a0edac19aa99
shaho1090 <shaho.parvini@gmail.com>
2024-09-25 18:37:46 +0330

    lesson: 104, outputting content in the meal detail screen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,9 +1,32 @@
-import { Text } from "react-native";
+import { View, Image, Text, ScrollView } from "react-native";
+
+import { MEALS } from "../data/dummy-data";
+import MealDetails from "../components/MealDetails";
 
 function MealDetailScreen({ route }) {
   const mealId = route.params.mealId;
 
-  return <Text>This is the Meal Details {mealId}</Text>;
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  return (
+    <View>
+      <Image source={{ uri: selectedMeal.imageUrl }} />
+      <Text>{selectedMeal.title}</Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        affordability={selectedMeal.affordability}
+        complexity={selectedMeal.complexity}
+      />
+      <Text>Ingredients</Text>
+      {selectedMeal.ingredients.map((ingredient) => (
+        <Text key={ingredient}>{ingredient}</Text>
+      ))}
+      <Text>Steps</Text>
+      {selectedMeal.steps.map((step) => (
+        <Text key={step}>{step}</Text>
+      ))}
+    </View>
+  );
 }
 
 export default MealDetailScreen;

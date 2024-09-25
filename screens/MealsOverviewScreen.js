313dcfe797a84cf2edc0a6c5b0bf9d2198f2fefc
shaho1090 <shaho.parvini@gmail.com>
2024-09-25 11:12:11 +0330

    lesson: 99, displaying meals

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,11 +1,26 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet, FlatList } from "react-native";
 
+import MealItem from "../components/MealItem";
 import { MEALS } from "../data/dummy-data";
 
-function MealsOverviewScreeen() {
+function MealsOverviewScreeen({ route }) {
+  const catId = route.params.categoryId;
+
+  const dispalyedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text> Meals Overview Screen</Text>
+      <FlatList
+        data={dispalyedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }

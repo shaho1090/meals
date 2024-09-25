09adb8b9d7b4c2cd3b55fa51c83d0b050e2c33e9
shaho1090 <shaho.parvini@gmail.com>
2024-09-25 15:11:29 +0330

    lesson: 101, styling screen background using screenOptions and option

--- App.js
+++ App.js
@@ -13,9 +13,27 @@ export default function App() {
     <>
       <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
-          <Stack.Screen name="MealsOverview" component={MealsOverviewScreeen} />
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: "#351401" },
+            headerTintColor: "white",
+            contentStyle: { backgroundColor: "#3f2f25" },
+          }}
+        >
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreen}
+            options={{
+              title: "All Categories",
+            }}
+          />
+          <Stack.Screen
+            name="MealsOverview"
+            component={MealsOverviewScreeen}
+            options={{
+              title: "Meals Overview",
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>

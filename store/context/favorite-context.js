5eec4fccc762b4c3bfd05c881b8741bbd85b92b8
shaho1090 <shaho.parvini@gmail.com>
2024-09-28 11:25:18 +0330

    lesson: 116, managing app wide state using context

--- store/context/favorite-context.js
+++ store/context/favorite-context.js
@@ -7,7 +7,29 @@ export const FavoriteContext = createContext({
 });
 
 function FavoriteContextProvider({ children }) {
-  return <FavoriteContext.Provider>children</FavoriteContext.Provider>;
+    const [favoriteMealIds, setFavoriteMealIds] = useState([]);
+
+    function addFavorite(id) {
+      setFavoriteMealIds((currentFavIds) => [...currentFavIds, id]);
+    }
+
+    function removeFavorite(id) {
+      setFavoriteMealIds((currentFavIds) =>
+        currentFavIds.filter((mealId) => mealId !== id)
+      );
+    }
+
+    const value = {
+      ids: favoriteMealIds,
+      addFavorite: addFavorite,
+      removeFavorite: removeFavorite,
+    };
+
+    return (
+      <FavoriteContext.Provider value={value}>
+        children
+      </FavoriteContext.Provider>
+    );
 }
 
 export default FavoriteContextProvider;
